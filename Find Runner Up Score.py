2fc8e5978aecccb1c8549299830004ad36ba1c52
Saruul Ganbold <43958152+grimexploit@users.noreply.github.com>
2020-04-28 17:55:48 +0800

    destr

--- Find Runner Up Score.py
+++ Find Runner Up Score.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
-    arr = list(map(int, input().split())) #array buyu massive iig avch baina
+    arr = list(map(int, input().split())) 
     k = max(arr)	#tuhain array-d ugugdsun elementuudiin hamgiin ih utgiig k huvisagchid hadgalav.
     for i in range(n):
         if max(arr) == k : 	#davtalt guilgeed hervee tuhain array iin max element ni bidnii umnu hadgalsan k tai tentsuu baih ym bol
